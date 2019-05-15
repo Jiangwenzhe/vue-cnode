3f37af0e62a4bc96b164077bd761cedd0d57075c
Erzhuyijian <1821344102@qq.com>
2019-05-15 16:48:04 +0800

    添加 Header.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="header">
+    <div class="header-container clearfix">
+      <a href="/" class="brand">
+        <img src="../assets/cnodejs_light.svg" alt="">
+      </a>
+      <ul>
+        <li><a href="">首页</a></li>
+        <li><a href="">新手入门</a></li>
+        <li><a href="">API</a></li>
+        <li><a href="">关于</a></li>
+        <li><a href="">注册</a></li>
+        <li><a href="">登录</a></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style lang="scss" scoped>
+  .clearfix::after {
+    content: '';
+    display: block;
+    clear: both;
+  }
+  .header {
+    margin-bottom: 0;
+    z-index: 9;
+    width: 100%;
+    position: relative;
+    background: #444;
+    font-size: 13px;
+    padding: 4px;
+    .header-container {
+      width: 90%;
+      max-width: 1400px;
+      margin: 0 auto;
+      .brand {
+        width: 120px;
+        padding: 5px 20px;
+        height: 34px;
+        line-height: 34px;
+        float: left;
+        margin-left: -20px;
+        font-size: 20px;
+        img {
+          vertical-align: initial;
+        }
+      }
+      ul {
+        list-style: none;
+        float: right;
+        margin-right: 0;
+        li {
+          float: left;
+          a {
+            text-decoration: none;
+            color: #ccc;
+            padding: 10px 15px;
+          }
+          a:hover {
+            color: white;
+          }
+        }
+      }
+    }
+  }
+</style>
