1f84521eb7ed310186d3f62809510475ca6dfd1b
Erzhuyijian <1821344102@qq.com>
2019-05-16 19:56:04 +0800

    分离出Loading条

---
+++ src/components/Loading.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="loading">
+      <span class="iconfont icon-loading"></span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Loading'
+}
+</script>
+
+<style lang="scss">
+  .loading {
+      text-align: center;
+      @keyframes rotate {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+      }
+      .iconfont {
+        font-size: 30px;
+        color: rgba(0,0,0,.3);
+        display: inline-block;
+        animation: 1s rotate linear infinite;
+      }
+    }
+</style>
